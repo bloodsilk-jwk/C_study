0b6182d454f08e3a36172e9047bbe48d9d10147d
bloodsilk0 <100887534+bloodsilk-jwk@users.noreply.github.com>
2022-05-04 21:12:48 +0900

    week4

---
+++ week4/4week_1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int arrangement[11] = { 10,2,5 };
+	for (int count = 0; count < 10; count++)
+	{
+		printf("%d ", arrangement[count]);
+	}
+}
---
+++ week4/4week_2.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char str[10];
+	scanf("%s", str, 10);
+	printf("%s\n", str);
+}
